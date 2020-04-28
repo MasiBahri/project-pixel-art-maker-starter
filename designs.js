5c65214034f74b2f8c4ce2755358533b18927683
unknown <bahri.masoumeh@gmail.com>
2020-04-28 23:24:36 +0100

    initial commit

--- designs.js
+++ designs.js
@@ -1,10 +1,26 @@
-// Select color input
-// Select size input
+let form = document.querySelector('button');
+let table = document.querySelector("#pixelCanvas");
+const height = document.querySelector('#inputHeight');
+const width = document.querySelector('#inputWidth');
+const colorCode = document.querySelector('#colorPicker');
 
-// When size is submitted by the user, call makeGrid()
+// attach click listener to button
+// This function gets height and width from the form and draw table with these values
+form.addEventListener('click', function () {
+    table.innerHTML = ""
+    for (let i = 0; i < height.value; i++) {
+        let newRow = table.insertRow()
+        for (let j = 0; j < width.value; j++) {
+            let cell = newRow.insertCell(j);
+            cell.setAttribute("ID", `cell-${i}-${j}`)
+        }
+    }
+});
 
-function makeGrid() {
-
-// Your code goes here!
-
-}
+// attach click listener to table.
+// this function fill cell with color from code which was selected in colorPick component
+table.addEventListener('click', (e) => {
+    if (e.target.matches("td")) {
+        e.target.style.backgroundColor = colorCode.value;
+    }
+})
